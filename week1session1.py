3b9290ca563d3b8c8cd94dd02ec5f111255cc1a7
Graziella <grazib223@gmail.com>
2025-02-19 20:17:27 -0600

    added problem 6

--- week1session1.py
+++ week1session1.py
@@ -90,3 +90,17 @@ hunny_jars = []
 sum_honey(hunny_jars)
 print()
 
+# Problem 6: Double Trouble
+#   Help Winnie the Pooh double his honey! Write a function doubled() 
+#   that accepts a list of integers hunny_jars as a parameter and 
+#   multiplies each element in the list by two. Return the doubled list.
+
+def doubled(hunny_jars):
+    for i in range(len(hunny_jars)):
+        hunny_jars[i] *= 2
+    print(hunny_jars)
+
+hunny_jars = [1, 2, 3]
+doubled(hunny_jars)
+print()
+
