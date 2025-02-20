e2930b4024fbfca566dfa3fe967c904f91673aea
Graziella <grazib223@gmail.com>
2025-02-19 20:04:31 -0600

    added problem 4

--- week1session1.py
+++ week1session1.py
@@ -49,3 +49,24 @@ print_catchphrase("pooh")
 print_catchphrase("Eeyore")
 print()
 
+# Problem 4: Return Item
+#   Implement a function get_item() that accepts a 0-indexed list items 
+#   and a non-negative integer x and returns the element at index x in items. 
+#   If x is not a valid index of items, return None.
+
+def get_item(items, x):
+    if x <= len(items):
+        print(items[x]) 
+
+    else:
+        print("None")
+
+items = ["piglet", "pooh", "roo", "rabbit"]
+x = 2
+get_item(items, x)
+
+items = ["piglet", "pooh", "roo", "rabbit"]
+x = 5
+get_item(items, x)
+print()
+
