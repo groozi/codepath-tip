c5655e2b7a003c1197964eeb2f19cadc8d2b93f4
Graziella <grazib223@gmail.com>
2025-02-19 20:10:13 -0600

    added problem 4

--- week1session1.py
+++ week1session1.py
@@ -70,3 +70,23 @@ x = 5
 get_item(items, x)
 print()
 
+# Problem 5: Total Honey
+#   Winnie the Pooh wants to know how much honey he has. 
+#   Write a function sum_honey() that accepts a list of integers 
+#   hunny_jars and returns the sum of all elements in the list. 
+#   Do not use the built-in function sum().
+
+def sum_honey(hunny_jars):
+    sum = 0
+
+    for jar in hunny_jars:
+        sum += jar
+    print(sum)
+
+hunny_jars = [2, 3, 4, 5]
+sum_honey(hunny_jars)
+
+hunny_jars = []
+sum_honey(hunny_jars)
+print()
+
