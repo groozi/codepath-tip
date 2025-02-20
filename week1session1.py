92221369691e2e48a3133c3ca0071369eeee2ee5
Graziella <grazib223@gmail.com>
2025-02-19 19:24:27 -0600

    completed problems 1-3

--- week1session1.py
+++ week1session1.py
@@ -1,1 +1,51 @@
 # Breakout Room Problems for Week 1 Session 1: Group 35
+
+# Problem 1: Hundred Acre Wood
+#   Write a function welcome() that prints the string "Welcome to The Hundred Acre Wood!".
+def welcome():
+    print("Welcome to The Hundred Acre Wood!")
+
+welcome()
+print()
+
+# Problem 2: Greeting
+#       Write a function greeting() that accepts a single parameter, a string name, 
+#       and prints the string "Welcome to The Hundred Acre Wood <name>! My name is Christopher Robin."
+
+def greeting(name):
+    print(f"Welcome to The Hundred Acre Wood {name}! My name is Christopher Robin.")
+
+greeting("Graziella")
+print()
+
+# Problem 3: Catchphrase 
+#   Write a function print_catchphrase() that accepts a string character as a parameter
+#   and prints the catchphrase of the given character as outlined in the table below.
+
+#   Character	Catchphrase
+#       "Pooh"	            "Oh bother!"
+#       "Tigger"	        "TTFN: Ta-ta for now!"
+#       "Eeyore"	        "Thanks for noticing me."
+#       "Christopher Robin"	"Silly old bear."
+
+#   If the given character does not match one of the characters included above, print 
+#   "Sorry! I don't know <character>'s catchphrase!"
+
+def print_catchphrase(character):
+    if character == "Pooh":
+        print("Oh bother!")
+    elif character == "Tigger":
+        print("TTFN: Ta-ta for now!")
+    elif character == "Eeyore":
+        print("Thanks for noticing me.")
+    elif character == "Christopher Robin":
+        print("Silly old bear.")
+    else:
+        print(f"Sorry! I don't know {character}'s catchphrase!")
+
+print_catchphrase("Graziella")
+print_catchphrase("Tigger")
+print_catchphrase("pooh")
+print_catchphrase("Eeyore")
+print()
+
